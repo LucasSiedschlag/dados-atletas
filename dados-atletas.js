3da96eb5daa6fecade3cd15cfbbe2da7eb684eb1
Lucas Siedschlag <106562852+LucasSiedschlag@users.noreply.github.com>
2022-12-06 23:55:27 -0300

    Create dados-atletas.js

---
+++ dados-atletas.js
@@ -0,0 +1,14 @@
+// Declara o atleta
+const atleta = new Atleta("Cesar Abascal",
+    30, 80, 1.70,
+    [10, 9.34, 8.42, 10, 7.88]);
+
+// Exemplo de Saída
+// Nome: Cesar Abascal
+// Idade: 30
+// Peso: 80
+// Altura: 1.7
+// Notas: 10,9.34,8.42,10,7.88
+// Categoria: Adulto
+// IMC: 27.68166089965398
+// Média válida: 8.766666666666666
